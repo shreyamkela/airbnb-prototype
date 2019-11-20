56160850c9d97db94a1bd91a5e46df48438afa43
darshilpk3 <darshilpk3@gmail.com>
2019-11-19 23:22:18 -0800

    Started with backend. A normal /ping route

---
+++ backend/Property/src/goapi/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+)
+
+
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.Use(corsObj)
+	n.UseHandler(mx)
+	return n
+}
+
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+}
+
+// API Ping Handler (GET call)
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"Property MicroService version 1.0 alive!"})
+	}
+}
