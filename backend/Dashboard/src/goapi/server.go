321fecaa10582792c061b2c2da58b32318a10cab
kovurivinay <kovurivinay19@gmail.com>
2019-11-20 22:59:41 -0800

    Changed IP to connect to Mongo

--- backend/Dashboard/src/goapi/server.go
+++ backend/Dashboard/src/goapi/server.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json" 
+
 	"github.com/codegangsta/negroni"
-	"github.com/rs/cors"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"github.com/unrolled/render"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-) 
+)
 
 // Mongodb config
-var mongodb_server = "127.0.0.1:27017"
+var mongodb_server = "10.0.1.138:27017"
 var mongodb_database = "cmpe281_airbnb"
 var mongodb_collection_dashboard = "user_dashboard"
 
@@ -23,9 +24,9 @@ func NewServer() *negroni.Negroni {
 		IndentJSON: true,
 	})
 	corsObj := cors.New(cors.Options{
-        AllowedOrigins: []string{"*"},
-        AllowedMethods: []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
-        AllowedHeaders: []string{"Accept", "content-type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Accept", "content-type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
 	})
 	n := negroni.Classic()
 	mx := mux.NewRouter()
@@ -43,8 +44,8 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 // API Ping Handler (GET call)
@@ -69,9 +70,9 @@ func travelerProfileGetHandler(formatter *render.Render) http.HandlerFunc {
 		session.SetMode(mgo.Monotonic, true)
 		c := session.DB(mongodb_database).C(mongodb_collection_dashboard)
 		var result User_details
-		err = c.Find(bson.M{"userid" : user_id}).One(&result)
+		err = c.Find(bson.M{"userid": user_id}).One(&result)
 		if err != nil {
-			fmt.Println("Some error occured while getting the user details of the user : ",user_id)
+			fmt.Println("Some error occured while getting the user details of the user : ", user_id)
 			fmt.Println(err)
 		}
 		fmt.Println("User data:", result)
@@ -83,33 +84,33 @@ func travelerProfileGetHandler(formatter *render.Render) http.HandlerFunc {
 func travelerProfileUpdateHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		var user_details User_details
-		_ = json.NewDecoder(req.Body).Decode(&user_details)	
-		fmt.Println("\n------------ User details coming from frontend : \n",user_details)
+		_ = json.NewDecoder(req.Body).Decode(&user_details)
+		fmt.Println("\n------------ User details coming from frontend : \n", user_details)
 		// params := mux.Vars(req)
 		var user_id string = user_details.UserId
-		fmt.Println( "Update request for user ID: ", user_details.UserId )
+		fmt.Println("Update request for user ID: ", user_details.UserId)
 		session, err := mgo.Dial(mongodb_server)
-        if err != nil {
-				fmt.Println("Some error occured while connecting to the mongo server")
-                panic(err)
-        }
+		if err != nil {
+			fmt.Println("Some error occured while connecting to the mongo server")
+			panic(err)
+		}
 		defer session.Close()
 		session.SetMode(mgo.Monotonic, true)
 		c := session.DB(mongodb_database).C(mongodb_collection_dashboard)
-		filter := bson.M{"userid" : user_id}
+		filter := bson.M{"userid": user_id}
 		// change := bson.M{"$set": user_details}
 		change := bson.M{
 			"$set": bson.M{
-				"address" : user_details.Address,
-				"userid" : user_details.UserId,
-				"firstname" : user_details.Firstname,
-				"lastname" : user_details.Lastname,
-				"phonenumber" : user_details.PhoneNumber,
-				"cardnumber" : user_details.CardNumber,
+				"address":     user_details.Address,
+				"userid":      user_details.UserId,
+				"firstname":   user_details.Firstname,
+				"lastname":    user_details.Lastname,
+				"phonenumber": user_details.PhoneNumber,
+				"cardnumber":  user_details.CardNumber,
 			},
 		}
 		var result1 User_details
-		err1 := c.Find(bson.M{"userid" : user_id}).One(&result1)
+		err1 := c.Find(bson.M{"userid": user_id}).One(&result1)
 		if err1 != nil {
 			fmt.Println("No entry exists for this user, thus inserting the data")
 			err = c.Insert(user_details)
@@ -124,7 +125,7 @@ func travelerProfileUpdateHandler(formatter *render.Render) http.HandlerFunc {
 			}
 		}
 		var result User_details
-		err = c.Find(bson.M{"UserId" : user_id}).One(&result)
+		err = c.Find(bson.M{"UserId": user_id}).One(&result)
 		fmt.Println("User details updated : \n", result)
 		formatter.JSON(w, http.StatusOK, "User details updated successfully")
 	}
